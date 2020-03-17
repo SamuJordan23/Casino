5d3ae31175bde6e8ec9ff54e6b18774d0652a9d2
SamuJordan23 <franchisamuele@gmail.com>
2020-03-17 12:03:08 +0100

    Aggiunto il banco come avversario

--- Blackjack.java
+++ Blackjack.java
@@ -4,11 +4,23 @@ public class Blackjack {
 
 	public static void play() {
 		MazzoPoker m = new MazzoPoker();
-		Scanner in=new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		Random r = new Random();
 		
 		System.out.println("- Blackjack -");
 		do {
+			int banco = banco();
+			if(banco == 21) {
+				System.out.println("Il banco ha vinto facendo BlackJack\n");
+				System.out.print("Vuoi giocare ancora? (s/n): ");
+				char giocaAncora = in.nextLine().charAt(0);
+				if(giocaAncora != 's' && giocaAncora != 'S') {
+					System.out.println("Ciao, torna presto!");
+					break;
+				}
+				else
+					continue;
+			}
 			int punteggio = 0;
 			int n = r.nextInt(51); //Posizione casuale nel mazzo
 			System.out.println("Le tue carte\n " + m.cartaCasuale(n)); //Stampa la carta che è uscita casualmente
@@ -49,11 +61,18 @@ public class Blackjack {
 					default: break;
 				}
 				
-				if(punteggio > 21)
+				if(punteggio == 21)
+					System.out.println("Hai vinto facendo BlackJack!");
+				else if(punteggio > 21)
 					System.out.println("Hai sballato con un punteggio di " + punteggio + "\n");
+				else if(banco > 21)
+					System.out.println("Il banco ha sballato con " + banco + ", hai vinto con un punteggio di " + punteggio + "\n");
+				else if(punteggio > banco)
+					System.out.println("Hai vinto con un punteggio di " + punteggio + " contro il banco che ha fatto " + banco + "\n");
+				else if(punteggio == banco)
+					System.out.println("Hai pareggiato con un punteggio di " + punteggio + "\n");
 				else
-					System.out.println("Hai concluso con un punteggio di " + punteggio + "\n");
-				
+					System.out.println("Hai perso con un punteggio di " + punteggio + " contro il banco che ha fatto " + banco + "\n");
 			}
 
 			System.out.print("Vuoi giocare ancora? (s/n): ");
@@ -68,6 +87,31 @@ public class Blackjack {
 		} while(true);
 		
 		in.close();
+	}
+	
+	private static int banco() {
+		MazzoPoker m = new MazzoPoker();
+		Random r = new Random();
+		
+		int punteggio = 0;
+		int n = r.nextInt(51);
+		System.out.println("Le carte del banco\n " + m.cartaCasuale(n) + "\n Carta coperta\n");
+		punteggio += m.valoreCarta(n);
+		n = r.nextInt(51);
+		if(m.valoreCarta(n) == 11 && punteggio >= 11)
+			punteggio += 1;
+		else
+			punteggio += m.valoreCarta(n);
+		
+		while(punteggio < 17) {
+			n = r.nextInt(51);
+			if(m.valoreCarta(n) == 11 && punteggio >= 11)
+				punteggio += 1;
+			else
+				punteggio += m.valoreCarta(n);
+		}
+		
+		return punteggio;
 	}
 
 }
